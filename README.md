5fa78c757cab8e159f41634457166c5ff165633d
Ross Bernstein <rb2540@nyu.edu>
2020-03-05 01:59:34 -0500

    Added files

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# reddit-prediction
+# CONTENTS OF THIS FILE
+---------------------
+   
+ * Introduction
+ * Requirements
+ * Configuration
+
+
+# INTRODUCTION
+------------
+
+This script scrapes Reddit submissions in real-time, from a list of specified subreddits, and gets updated
+values for the fields of each submission at specified time intervals. The script creates two CSV files. 
+The first file, called "stream_[date]_[time]" contains every unique submission created while the script was 
+running, on a separate row. The second file, called "score_[date]_[time]", contains a separate row for each 
+submission in the first file, at a given time interval.
+
+
+# REQUIREMENTS
+------------
+
+This script was written in Python 3.6 and utilizes PRAW, the Python Reddit API Wrapper. PRAW can be installed
+from the command line with pip.
+
+A consistent internet connection must be maintained at all times while running this script. Re-running will 
+cause the script to start over and create two new csv files. It will not continue checking submissions from a 
+previous run for which it did not collect the full 24-hours of data.
+
+Accessing the Reddit API and using this script requires a Reddit account. After making a reddit account, you
+must also create a Reddit application by going to the "apps" tab in Preferences. The application-type selected
+should be "script". Once your app is created, the following values must be used for the script:
+
+* client_id
+* client_secret
+* username
+* password
+
+These values are entered when creating a Reddit instance, as in the load_reddit() function in this script. 
+A "user_agent" must also entered. This is a unique identifier that helps Reddit determine the source of network
+requests. The PRAW's documentation suggest the following format for user agent:
+
+* <platform>:<app ID>:<version string> (by /u/<Reddit username>)
+
+For example:
+
+* windows:myredditapp:v1.2.3 (by /u/RedditUser)
+
+
+# CONFIGURATION
+-------------
+ 
+ * Configure the reddit instance using the above instructions
+
+ * If different time intervals for checking submissions are required, adjust the times in the 'mins_list' 
+   variable as needed
+
+ * Selecting the subreddits to scrape can be specified as a command-line argument, or hard-coded. This is 
+   set in main(). The script already uses hard-coded subreddits. Simply change the 'subred' string to contain
+   subreddits you wish to scrape, with each subreddit separated by a '+'. The command-line method is included
+   but commented out. To use this method, simply un-comment the line '#subred = sys.argv[1]' and remove the 
+   following line. The subreddits will then be selected from the command-line when running the script.   
+   For example:
+
+   C:\>python3 Reddit_Submission_Scraper.py AskReddit+Pics+Gifs+Poliitics
+
---
+++ Reddit_Submission_Scraper.py
@@ -0,0 +1,141 @@
+''' Reddit Submission Scraper -- This script gets every new submission from the specified
+subreddits and creates two csv files. The first file contains every unique submission created
+while the script was running, each on a separate row. The second file contains every submission
+in the first file, with separate rows for the updated values at each time interval (i.e, score, num_comments, etc.).
+'''
+
+import traceback
+import multiprocessing as mp
+import csv
+import queue
+import sys
+import praw
+import time
+from datetime import datetime, timedelta, timezone
+
+fields = ['id', 'author', 'score', 'title', 'selftext', '_comments_by_id',
+          'created_utc', 'num_comments', 'num_crossposts',
+          'over_18', 'permalink', 'url', 'subreddit', 'subreddit_id',
+          'subreddit_subscribers', 'stickied',
+          'gilded', 'is_self']
+all_fields = ['comment_karma', 'link_karma', 'time_retrieved_utc',
+              'minutes'] + fields
+
+# Time intervals to check each submission at:
+mins_list = [5, 10, 15, 30, 60] + [120 + 60 * k for k in range(23)]
+
+
+def load_reddit():
+# The credentials in the Reddit instance below must be filled in, or stored in a separate .ini file
+    reddit = praw.Reddit(user_agent='',
+                         client_id='',
+                         client_secret='',
+                         username='',
+                         password='')
+    return reddit
+
+
+# Store fields for current submission in a dict
+def sub_to_dict(sub, t, minutes, use_karma = False):
+    global fields
+    sub_dict = dict()
+    for field in fields:
+        sub_dict[field] = getattr(sub, field)
+    sub_dict['time_retrieved_utc'] = int(t.timestamp())
+    sub_dict['minutes'] = minutes
+    sub_dict['comment_karma'] = sub.author.comment_karma if use_karma else ''
+    sub_dict['link_karma'] = sub.author.link_karma if use_karma else ''
+    return sub_dict
+
+
+# Write submissions/fields to rows of the csv
+def bulk_write_subs(reddit, writer, fullnames, q_entries, t) :
+    global mins_list
+    try :
+        subs = reddit.info(fullnames)
+        for i,sub in enumerate(subs) :
+            writer.writerow(sub_to_dict(sub,t,mins_list[q_entries[i][1]]))
+    except Exception as e:
+        exc_info = sys.exc_info()
+        traceback.print_exception(*exc_info)
+        print('Info Exception', e)
+        raise e
+
+
+# Get new submissions
+def stream_processor(subred, q):
+    global all_fields, mins_list
+    reddit = load_reddit()
+    fname = 'stream_%s_%s.csv' % (subred, datetime.now().strftime('%Y-%m-%d_%H%M%S'))
+    has_started = False
+    try:
+        with open(fname, 'w', newline='', encoding='utf-8', buffering=1) as out_file:
+            writer = csv.DictWriter(out_file, fieldnames=all_fields)
+            writer.writeheader()
+            for submission in reddit.subreddit(subred).stream.submissions():
+                t = datetime.now().astimezone()
+                sub_dict = sub_to_dict(submission, t, 0, use_karma=True)
+                t_c = datetime.utcfromtimestamp(sub_dict['created_utc'])
+                t_c = t_c.replace(tzinfo=timezone.utc)
+                t_c = t_c.astimezone()
+                if t_c + timedelta(minutes=1) > t:
+                    has_started = True
+                if has_started:
+                    writer.writerow(sub_dict)
+                    q_entry = (t + timedelta(minutes=mins_list[0]), 0, sub_dict['id'], submission.fullname)
+                    q.put(q_entry)
+    except Exception as e:
+        exc_info = sys.exc_info()
+        traceback.print_exception(*exc_info)
+        print('Stream Exception', e)
+        raise e
+
+
+# Get updated fields of old submissions
+def score_processor(subred, q):
+    global all_fields, mins_list
+    pq = queue.PriorityQueue()
+    reddit = load_reddit()
+    fname = 'score_%s_%s.csv' % (subred, datetime.now().strftime('%Y-%m-%d_%H%M%S'))
+    try:
+        with open(fname, 'w', newline='', encoding='utf-8', buffering=1) as out_file:
+            writer = csv.DictWriter(out_file, fieldnames=all_fields)
+            writer.writeheader()
+            while True:
+                t = datetime.now().astimezone()
+                if not q.empty():
+                    pq.put(q.get())
+                if not pq.empty():
+                    q_entries = []
+                    while len(q_entries) < 100 and pq.queue[0][0] < t:
+                        q_entries.append(pq.get())
+                    if q_entries:
+                        fullnames = [str(a[3]) for a in q_entries]
+                        print('fullnames',fullnames)
+                        sys.stdout.flush()
+                        bulk_write_subs(reddit, writer, fullnames, q_entries, t)
+
+                        for (q_t, idx, sub_id, fullname) in q_entries :
+                            if idx + 1 < len(mins_list):
+                                delta = timedelta(minutes=mins_list[idx + 1] - mins_list[idx])
+                                q_entry = (t + delta, idx + 1, sub_id, fullname)
+                                pq.put(q_entry)
+    except Exception as e:
+        exc_info = sys.exc_info()
+        traceback.print_exception(*exc_info)
+        print('Score Exception', e)
+        raise e
+
+
+def main():
+    q = mp.Queue()
+    # Subreddits can be entered as a command line argument, or hard-coded.
+    # In either case, multiple subreddits should be separated by '+' as the line below shows.
+    #subred = sys.argv[1]
+    subred = 'AskReddit+Pics+Gifs+Videos+WorldNews+Funny+Aww+gaming'
+    mp.Process(target=score_processor, args=(subred, q)).start()
+    mp.Process(target=stream_processor, args=(subred, q)).start()
+
+
+if __name__ == "__main__":
+    main()
